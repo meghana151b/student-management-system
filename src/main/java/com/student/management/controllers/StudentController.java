536777012330cf55a0845bc69b6a0224a81fd907
meghana151b <b11meghana@gmail.com>
2025-04-17 23:47:40 +0530

    Improved Student Management System with UI/backend changes

--- src/main/java/com/student/management/controllers/StudentController.java
+++ src/main/java/com/student/management/controllers/StudentController.java
@@ -3,20 +3,18 @@ package com.student.management.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.student.management.entities.Student;
 import com.student.management.services.StudentService;
 
 
-@RestController
+@Controller
 public class StudentController {
 	
 	@Autowired
@@ -24,31 +22,37 @@ public class StudentController {
      @PostMapping("/addStudent")
      public String addStudent(@ModelAttribute Student st){
 	service.addStudent(st);
-	return"Added Student from postman";
+	return"index";
 }
+     
+     //org.springframework.ui.Model;
      @GetMapping("/searchStudent")
-     public Student searchStudent(@RequestParam int roll) {
-    	return service.searchStudent(roll);
+     public String searchStudent(@RequestParam int roll,Model model) {
+    	Student st= service.searchStudent(roll);
+    	model.addAttribute("student",st);
+    	return "displayStudent";
      }
      @PostMapping("/updateStudent")
      public String updateStudent(@ModelAttribute Student st) {
     	 service.updateStudent(st);
-    	 return "Updated Student";
+    	 return "index";
      }
      @GetMapping("/deleteStudent")
     	 public String deleteStudent(@RequestParam int roll) {
     		 service.deleteStudent(roll);
-    		 return "Student deleted successfully";
+    		 return "index";
      }
      @GetMapping("/fetchAllStudents")
-     public List<Student> fetchAllStudents(){
-    	 return service.fetchAllStudents();
+     public String fetchAllStudents(Model model){
+    	 List<Student> stList = service.fetchAllStudents();
+    	 model.addAttribute("studentList", stList);
+    	 return "displayAllStudents";
      }
      
      @GetMapping("/deleteAllStudents")
      public String deleteAllStudents() {
     	 service.deleteAllStudents();
-    	 return "Deleted all students";
+    	 return "index";
      }
 
 }
